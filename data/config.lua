2d00e874d87b310f588571551d912c6d204c73d9
Ameeralasdee <ali.gta307@gmail.com>
2017-02-24 09:17:29 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,25 @@
+do local _ = {
+  admins = {
+    {
+      186907404,
+      "@Musa07714251059m"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools"
+  },
+  info_text = "💡Welcome To\n Ameeralasdee  \nFor More Information Subscribe To The Channel @Ameeralasdee \n\n💡Dev \n\n💡 Dev Bot @ll60Kllbot\n\n💡 channel @virus29\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    252164262
+  }
+}
+return _
+end
